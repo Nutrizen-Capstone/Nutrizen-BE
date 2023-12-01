4e12d7c1015ea692d2ece8d9949417272a5471a9
aditakbars <aditakbars33@gmail.com>
2023-12-01 20:27:08 +0700

    logout done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import db from "./config/db.js";
 import router from "./routes/routes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ try {
     console.log(error);
 }
 
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }))
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
