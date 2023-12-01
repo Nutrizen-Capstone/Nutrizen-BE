21bedef4331f60abe99d7794eed4735e112eaccc
aditakbars <aditakbars33@gmail.com>
2023-12-01 16:10:31 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import db from "./config/db.js";
+import router from "./routes/routes.js";
+
+const app = express();
+
+try {
+    await db.authenticate();
+    console.log('Database Connected!');
+} catch (error) {
+    console.log(error);
+}
+
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => console.log('Server running at port 5000'));
