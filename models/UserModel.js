af16b45621f67458240b490040fc62f5c06e6b29
aditakbars <aditakbars33@gmail.com>
2023-12-10 15:21:57 +0700

    adding uuid as user id

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,25 +1,76 @@
 import { Sequelize } from "sequelize";
+import { v4 as uuidv4 } from 'uuid';
 import db from "../config/db.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
 const Users = db.define('users', {
-    name:{
-        type: DataTypes.STRING
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: () => uuidv4(), // Set nilai default menggunakan UUID
+        allowNull: false,
+        unique: true, // Jadikan kolom ini unique
     },
-    email:{
-        type: DataTypes.STRING
+    name: {
+        type: DataTypes.STRING,
     },
-    password:{
-        type: DataTypes.STRING
+    email: {
+        type: DataTypes.STRING,
     },
-    refresh_token:{
-        type: DataTypes.STRING
+    password: {
+        type: DataTypes.STRING,
     },
-},{
-    freezeTableName:true
+    refresh_token: {
+        type: DataTypes.STRING,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    photoUrl: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://source.unsplash.com/100x100?user',
+    },
+    birthDate: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.fn('current_timestamp'),
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    gender: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    weight: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0,
+    },
+    height: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0,
+    },
+    activity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    goal: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    isDataCompleted: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+}, {
+    freezeTableName: true,
 });
 
 export default Users;
-
-
