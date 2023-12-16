08b83e46d0d1eef3d7483287beb0b8f08241a2b1
aditakbars <aditakbars33@gmail.com>
2023-12-16 23:39:24 +0700

    finally connected to cloud sql

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('nutridb','root', '',{
-    host: "localhost",
-    dialect: "mysql"
+dotenv.config();
+
+const db = new Sequelize({
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 export default db; 
