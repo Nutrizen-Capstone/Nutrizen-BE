082e821596ed910e617717ec8638d428a63c5b7b
aditakbars <aditakbars33@gmail.com>
2023-12-17 21:37:06 +0700

    fixing scan model

--- models/ScanModel.js
+++ models/ScanModel.js
@@ -2,12 +2,13 @@ import { DataTypes, Sequelize } from 'sequelize';
 import { v4 as uuidv4 } from 'uuid';
 import db from '../config/db.js';
 
-const ScanHistory = db.define('scan_history', {
+const ScanHistory = db.define('scan_histories', {
     historyId: {
         type: DataTypes.UUID,
         primaryKey: true,
-        defaultValue: uuidv4(),
+        defaultValue: () => uuidv4(),
         allowNull: false,
+        unique: true
     },
     userId: {
         type: DataTypes.STRING(255),
