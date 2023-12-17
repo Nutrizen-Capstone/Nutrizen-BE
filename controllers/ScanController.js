c1a6176174773f0ef0482100a2b2494eac8e3788
aditakbars <aditakbars33@gmail.com>
2023-12-17 21:28:28 +0700

    adding scan controller

---
+++ controllers/ScanController.js
@@ -0,0 +1,52 @@
+import ScanHistory from '../models/ScanModel.js';
+
+export const addScanHistory = async (req, res) => {
+    try {
+        const { userId, nameFood, eatTime, calorie, portion, total } = req.body;
+
+        const newScanHistory = await ScanHistory.create({
+            userId,
+            nameFood,
+            eatTime,
+            calorie,
+            portion,
+            total,
+        });
+
+        res.json({
+            error: false,
+            message: "success",
+            scanResult: newScanHistory
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: true,
+            message: "Terjadi kesalahan server"
+        });
+    }
+};
+
+export const getScanHistoryByUserId = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const scanHistory = await ScanHistory.findAll({
+            where: {
+                userId,
+            },
+        });
+
+        res.json({
+            error: false,
+            message: "success",
+            history: scanHistory
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: true,
+            message: "Terjadi kesalahan server"
+        });
+    }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import  { getUsers, Login, Logout, Register, getUser, completeProfile, editProfile, Welcome } from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
+import { addScanHistory, getScanHistoryByUserId } from "../controllers/ScanController.js";
 
 const router = express.Router();
 
@@ -14,5 +15,11 @@ router.get('/users', verifyToken, getUsers);
 router.get('/users/:id', verifyToken, getUser);
 router.put('/profile/:id', verifyToken, completeProfile);
 router.put('/profile/:id/edit', verifyToken, editProfile);
+
+// Rute untuk menambahkan catatan scan history
+router.post('/scan', addScanHistory);
+
+// Rute untuk mendapatkan catatan scan history berdasarkan userId
+router.get('/scan/:userId', getScanHistoryByUserId);
 
 export default router
