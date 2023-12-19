afa91605d20b728528c0f6ee35d049316ada3933
aditakbars <aditakbars33@gmail.com>
2023-12-19 18:58:40 +0700

    fixing timezone

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const db = new Sequelize({
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    timezone: '+07:00'
 });
 
 export default db; 
